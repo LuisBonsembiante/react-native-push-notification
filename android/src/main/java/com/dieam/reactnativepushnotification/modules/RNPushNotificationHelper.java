17c3c0b73debb7f0aed6d8012abeabe46a2a0a5b
Luis Bosnembiante <luisbonsembiante@gmail.com>
2019-07-25 15:25:53 -0300

    gradle

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -281,7 +281,7 @@ public class RNPushNotificationHelper {
             intent.putExtra("notification", bundle);
 
             if (!bundle.containsKey("playSound") || bundle.getBoolean("playSound")) {
-                Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+                Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
                 String soundName = bundle.getString("soundName");
                 if (soundName != null) {
                     if (!"default".equalsIgnoreCase(soundName)) {
